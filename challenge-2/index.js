677657d4326510c7ef4a14fa26a2a2f833c696ff
csc32 <sanzonettycar@gmail.com>
2024-12-14 18:52:56 -0400

    refactor challenge 2

--- challenge-2/index.js
+++ challenge-2/index.js
@@ -1,10 +1,9 @@
 function createFrame(names) {
-  const maxNameLength = names.reduce((a, b) =>
-    b.length > a.length ? b : a,
-  ).length;
-
+  const maxNameLength = Math.max(...names.map((el) => el.length));
+  console.log(maxNameLength);
   let rectangle = "*".repeat(maxNameLength + 4) + "\n";
 
+  console.log(rectangle);
   for (let i = 0; i < names.length; i++) {
     rectangle += `* ${names[i] + " ".repeat(maxNameLength - names[i].length)} *\n`;
   }
